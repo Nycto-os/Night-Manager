efacea1f9d06f0606d0dde1a41d96fed3f68b6a5
SuchitraShankar07 <suchitrashankar07@gmail.com>
2024-10-16 16:31:42 +0530

    fixed up terminal fn

--- software_manager.c
+++ software_manager.c
@@ -1,20 +1,38 @@
 #include <gtk/gtk.h>
 #include <vte/vte.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 GtkWidget *package_list;
 GtkWidget *details_label;
 GtkWidget *terminal;
 GtkWidget *toggle_terminal_button;
-gboolean terminal_visible = FALSE; 
+gboolean terminal_visible = FALSE;
 
-char *packages[] = {"App 1", "App 2", "App 3", "App 4"};
-char *details[] = {
-    "App 1: A simple app.\nVersion: 1.0.0\nSize: 10MB",
-    "App 2: A game app.\nVersion: 2.1.0\nSize: 50MB",
-    "App 3: A utility app.\nVersion: 0.9.3\nSize: 5MB",
-    "App 4: A multimedia app.\nVersion: 3.0.5\nSize: 100MB"
-};
+void on_package_selected(GtkTreeView *tree_view, GtkTreePath *path, GtkTreeViewColumn *col, gpointer user_data);
+void on_install_clicked(GtkWidget *widget, gpointer data);
+void on_uninstall_clicked(GtkWidget *widget, gpointer data);
+void on_toggle_terminal_clicked(GtkWidget *widget, gpointer data);
+void populate_package_list(GtkListStore *list_store);
+
+void fetch_available_packages(GtkListStore *list_store) {
+    FILE *fp;
+    char output[1024];
+    
+    fp = popen("pacman -Slq", "r");
+    if (fp == NULL) {
+        perror("Failed to run pacman command");
+        return;
+    }
+
+    GtkTreeIter iter;
+    while (fgets(output, sizeof(output), fp) != NULL) {
+        output[strcspn(output, "\n")] = 0; // Remove newline
+        gtk_list_store_append(list_store, &iter);
+        gtk_list_store_set(list_store, &iter, 0, output, -1);
+    }
+    pclose(fp);
+}
 
 void on_package_selected(GtkTreeView *tree_view, GtkTreePath *path, GtkTreeViewColumn *col, gpointer user_data) {
     GtkTreeModel *model;
@@ -24,30 +42,76 @@ void on_package_selected(GtkTreeView *tree_view, GtkTreePath *path, GtkTreeViewC
     model = gtk_tree_view_get_model(tree_view);
     if (gtk_tree_model_get_iter(model, &iter, path)) {
         gtk_tree_model_get(model, &iter, 0, &selected_package, -1);
-
-        // Find and display package details
-        for (int i = 0; i < 4; i++) {
-            if (g_strcmp0(selected_package, packages[i]) == 0) {
-                gtk_label_set_text(GTK_LABEL(details_label), details[i]);
-                break;
-            }
-        }
+        
+        gchar *details = g_strdup_printf("Package: %s\nVersion: 1.0.0\nSize: 10MB", selected_package);
+        gtk_label_set_text(GTK_LABEL(details_label), details);
+        g_free(details);
         g_free(selected_package);
     }
 }
 
+// // Install button callback
+// void on_install_clicked(GtkWidget *widget, gpointer data) {
+//     g_print("Installing package...\n");
+//     const char *command = "echo 'Installing package...' && sudo pacman -S --noconfirm <package-name>\n";
+//     vte_terminal_feed_child(VTE_TERMINAL(terminal), command, -1);
+// }
+
+// // Uninstall button callback
+// void on_uninstall_clicked(GtkWidget *widget, gpointer data) {
+//     g_print("Uninstalling package...\n");
+//     const char *command = "echo 'Uninstalling package...' && sudo pacman -R --noconfirm <package-name>\n";
+//     vte_terminal_feed_child(VTE_TERMINAL(terminal), command, -1);
+// }
+
 void on_install_clicked(GtkWidget *widget, gpointer data) {
     g_print("Installing package...\n");
-    vte_terminal_feed_child(VTE_TERMINAL(terminal), "echo 'Installing package...'\n", -1);
-  // real code to install packages, to be inserted here  ps: look into your gdocs to find more details.
+    
+    // The command that will be executed when the package is installed
+    const char *command[] = { "/bin/sh", "-c", "echo 'Installing package...' && sudo pacman -S --noconfirm <package-name>", NULL };
+    
+    // Spawn the command in the embedded terminal
+    vte_terminal_spawn_async(
+        VTE_TERMINAL(terminal),       // The VTE terminal widget
+        VTE_PTY_DEFAULT,              // Use the default pseudo-terminal
+        NULL,                         // Working directory (NULL uses current directory)
+        (char **)command,             // Command to execute
+        NULL,                         // Environment (NULL uses current environment)
+        G_SPAWN_DEFAULT,              // Spawn flags
+        NULL,                         // Child setup function (not needed here)
+        NULL,                         // User data for the setup function
+        NULL,                         // Cancellable object (not needed)
+        -1,                           // Timeout for the operation (-1 means no timeout)
+        NULL,                         // GCancellable (not needed)
+        NULL,                         // Callback for process completion (not needed here)
+        NULL                          // User data for the callback
+    );
 }
 
-// Uninstall button callback
 void on_uninstall_clicked(GtkWidget *widget, gpointer data) {
     g_print("Uninstalling package...\n");
-    vte_terminal_feed_child(VTE_TERMINAL(terminal), "echo 'Uninstalling package...'\n", -1);
-    //likewise but for uninstall
+    
+    // Prepare the command to be executed in the terminal
+    const char *command[] = { "/bin/sh", "-c", "echo 'Uninstalling package...' && sudo pacman -R --noconfirm <package-name>", NULL };
+    
+    // Spawn the command in the embedded terminal
+    vte_terminal_spawn_async(
+        VTE_TERMINAL(terminal),       // The VTE terminal widget
+        VTE_PTY_DEFAULT,              // Use the default pseudo-terminal
+        NULL,                         // Working directory (NULL uses current directory)
+        (char **)command,             // Command to execute
+        NULL,                         // Environment (NULL uses current environment)
+        G_SPAWN_DEFAULT,              // Spawn flags
+        NULL,                         // Child setup function (not needed here)
+        NULL,                         // User data for the setup function (unnecessary)
+        NULL,                         // Cancellable object (not needed)
+        -1,                           // Timeout for the operation (-1 means no timeout)
+        NULL,                         // GCancellable (not needed)
+        NULL,                         // Callback for process completion (not needed here)
+        NULL                          // User data for the callback
+    );
 }
+
 
 // Toggle terminal visibility
 void on_toggle_terminal_clicked(GtkWidget *widget, gpointer data) {
@@ -61,16 +125,8 @@ void on_toggle_terminal_clicked(GtkWidget *widget, gpointer data) {
     }
 }
 
-void populate_package_list(GtkListStore *list_store) {
-    GtkTreeIter iter;
-    for (int i = 0; i < 4; i++) {
-        gtk_list_store_append(list_store, &iter);
-        gtk_list_store_set(list_store, &iter, 0, packages[i], -1);
-    }
-}
-
 int main(int argc, char *argv[]) {
-    GtkWidget *window, *vbox, *button_box, *install_button, *uninstall_button;
+    GtkWidget *window, *vbox, *scrolled_window, *button_box, *install_button, *uninstall_button;
     GtkListStore *list_store;
     GtkCellRenderer *renderer;
     GtkTreeViewColumn *column;
@@ -85,20 +141,25 @@ int main(int argc, char *argv[]) {
     vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
-    list_store = gtk_list_store_new(1, G_TYPE_STRING);
-    populate_package_list(list_store);
+    scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    gtk_box_pack_start(GTK_BOX(vbox), scrolled_window, TRUE, TRUE, 0);
 
-    // Create the package list view
+    // Package list
+    list_store = gtk_list_store_new(1, G_TYPE_STRING);
+    fetch_available_packages(list_store);  /
+
     package_list = gtk_tree_view_new_with_model(GTK_TREE_MODEL(list_store));
     renderer = gtk_cell_renderer_text_new();
     column = gtk_tree_view_column_new_with_attributes("Available Packages", renderer, "text", 0, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(package_list), column);
-    gtk_box_pack_start(GTK_BOX(vbox), package_list, TRUE, TRUE, 0);
+    gtk_container_add(GTK_CONTAINER(scrolled_window), package_list);
 
     g_signal_connect(package_list, "row-activated", G_CALLBACK(on_package_selected), NULL);
 
     details_label = gtk_label_new("Select a package to view details.");
     gtk_box_pack_start(GTK_BOX(vbox), details_label, FALSE, FALSE, 0);
+
 
     button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
     gtk_box_pack_start(GTK_BOX(vbox), button_box, FALSE, FALSE, 0);
@@ -111,19 +172,18 @@ int main(int argc, char *argv[]) {
     g_signal_connect(uninstall_button, "clicked", G_CALLBACK(on_uninstall_clicked), NULL);
     gtk_container_add(GTK_CONTAINER(button_box), uninstall_button);
 
+    // widget for terminal
     terminal = vte_terminal_new();
     vte_terminal_spawn_async(VTE_TERMINAL(terminal), VTE_PTY_DEFAULT, NULL, NULL, NULL, 0, NULL, NULL, NULL, -1, NULL, NULL, NULL);
     gtk_box_pack_start(GTK_BOX(vbox), terminal, TRUE, TRUE, 0);
-    gtk_widget_hide(terminal); 
+    gtk_widget_hide(terminal);  // by default this is hidden
 
+    // allowing users to toggle terminal based on tastes
     toggle_terminal_button = gtk_button_new_with_label("Show Terminal");
     g_signal_connect(toggle_terminal_button, "clicked", G_CALLBACK(on_toggle_terminal_clicked), NULL);
     gtk_box_pack_start(GTK_BOX(vbox), toggle_terminal_button, FALSE, FALSE, 0);
 
-    
     gtk_widget_show_all(window);
-
-    
     gtk_main();
 
     return 0;
